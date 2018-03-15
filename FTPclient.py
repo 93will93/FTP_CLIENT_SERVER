cc7538653bec086625a423adfdc4ca16471ee016
Unknown <wbecerra1@gmail.com>
2018-03-15 21:25:22 +0200

    NOthing changed

--- FTPclient.py
+++ FTPclient.py
@@ -11,7 +11,6 @@ ENTERING_PASV_MODE_CODE = 227
 
 
 class FTPclient:
-
     def __init__(self, ftp_server, user='', password=''):
         self._ftp_server = ftp_server
         self._user = user
@@ -38,31 +37,49 @@ class FTPclient:
         code = message[:3]
         return int(code)
 
+    def closeDataPort(self):
+        self._data_port.close()
+        self._data_port = None
+
+    def createDataPortConnection(self):
+        self.pasv()
+
     def quit(self):
         self._tcp_cmd.transmit('QUIT' + CRLF)
         server_resp = self._tcp_cmd.receive()
         print(server_resp)
 
     def pasv(self):
-        self._tcp_cmd.transmit('PASV' + CRLF)
-        server_resp = self._tcp_cmd.receive(8192)
-
+        self._tcp_cmd.transmit('PASV ' + CRLF)
+        server_resp = self._tcp_cmd.receive()
+        print(str(server_resp))
         server_ip, self._data_port = self.pasvModeStringHandling(server_resp)
         self._tcp_data = TCP.TCP(self._ftp_server, self._data_port)
         return self._tcp_data
 
     def list(self):
-        self._tcp_cmd.transmit('LIST' + CRLF)
+        self._tcp_cmd.transmit('LIST ' + CRLF)
         print(self._tcp_cmd.receive())
         print(self._tcp_data.receive())
+        self._tcp_data.close()
+        print('Closing Data Port: ' + str(self._data_port))
 
     def retr(self, path):
-        self._tcp_cmd.transmit('RETR' + ' ' + path + CRLF)
-        print(self._tcp_cmd.receive())
-        x = self._tcp_data.receive()
-        f = open('Photo.scr', "w+")
-        f.write(x)
+        self.pasv()
+        self.createDataPortConnection()
+        self._tcp_cmd.transmit('TYPE I ' + CRLF)
+        print('Response to TYPE I: ' + str(self._tcp_cmd.receive(8192)))
+        self._tcp_cmd.transmit('RETR ' + ' ' + path + CRLF)
+        print('Response to RETR: ' + str(self._tcp_cmd.receive()))
+
+        data = self._tcp_data.receive()
+        while data:
+            data = data + self._tcp_data.receive()
+
+        f = open('Photo.scr', "wb")
+        f.write(data)
         f.close()
+        self.closeDataPort()
 
     def pwm(self):
         self._tcp_cmd.transmit(CRLF)
@@ -102,14 +119,21 @@ class FTPclient:
 
 # Testing the class works with an open ftp server
 if __name__ == '__main__':
-    client = FTPclient('ftp.uconn.edu', 'anonymous', 'anonymous@')
+    # Schools login-> ftp.ELEN4017.ug.eie.wits.ac.za user:group14  pass:engaqu4a
+    eie_ftp = 'ELEN4017.ug.eie.wits.ac.za'
+    eie_user = 'group14'
+    eie_pass = 'engaqu4a'
+    mirror_ftp = 'ftp.mirror.ac.za'
+
+    client = FTPclient(mirror_ftp)
+    # client = FTPclient(eie_ftp, eie_user, eie_pass)
     client.pasv()
     client.list()
 
     path = input('Please enter file path: ')
     # client.cwd(path)
-    client.list()
+    # client.list()
 
-    # client.retr(path)
-    client.pwm()
+    client.retr(path)
+    # client.pwm()
     client.quit()
