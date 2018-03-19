f66147cfcaca1680db077271da477a2d6aeead06
Unknown <wbecerra1@gmail.com>
2018-03-19 21:56:43 +0200

    Can Download Files-Saved to Working directory

--- PyQt/main.py
+++ PyQt/main.py
@@ -24,6 +24,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self._uploadedFile = None
         # Setting up check list
         self.cb_show_password.setChecked(False)
+        self.pb_upload.setValue(0)
+        self.pb_download.setValue(0)
 
         # Setting Up the labels
         f = self.lb_ftp_server_address.font()
@@ -33,13 +35,15 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.lb_ftp_server_address.setFont(f)
         self.lb_server_response.setFont(f)
         self.lb_action.setFont(f)
-        self.lb_pathto_moveto.setFont(f)
+        self.lb_movepath.setFont(f)
+        self.lb_upload_path.setFont(f)
+        self.lb_filename.setFont(f)
 
         # Setting up line edits (User Input textbox)
         self.le_ftp_server_address.setPlaceholderText('i.e ftp.mirror.ac.za')
         self.le_username.setPlaceholderText('hint: Your account username')
         self.le_password.setPlaceholderText('hint: Your account password')
-        self.le_pathto_moveto.setPlaceholderText('hint: ?/home/documents')
+        self.le_move.setPlaceholderText('hint: /home/documents')
         self.le_password.setEchoMode(PASSWORD_MODE)  # Hides the text
 
         # Setting up Combo Box
@@ -52,16 +56,12 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.comboBox_userAction.currentIndexChanged[str].connect(self.comboBox_userAction_handler)
         self.btn_proceed.pressed.connect(self.btn_proceed_handler)
         self.btn_loadfile.pressed.connect(self.btn_loadfile_handler)
-        self.btn_sendto_server.pressed.connect(self.btn_sendto_server_handler)
-        self.btn_moveto_path.pressed.connect(self.btn_moveto_path_handler)
+        self.btn_upload.pressed.connect(self.btn_upload_handler)
+        self.btn_move.pressed.connect(self.btn_move_handler)
+        # self.btn_upload.clicked.connect(self.uploadProgress)
+        self.btn_download.pressed.connect(self.btn_download_handler)
+        self.btn_saveto.pressed.connect(self.btn_saveto_handler)
 
-        # QTree
-        self.treeWidget.setColumnWidth(1, 150)
-        self.treeWidget.setColumnWidth(2, 150)
-        self.treeWidget.setIconSize(QSize(20, 20))
-        self.treeWidget.setRootIsDecorated(False)
-        self.treeWidget.setHeaderLabels(('Name', 'Size'))
-        self.treeWidget.header().setStretchLastSection(False)
 
 
     # LOADING TO SERVER HANDLER
@@ -75,7 +75,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             print("No filename")
 
-    def btn_sendto_server_handler(self):
+    def btn_upload_handler(self):
         path = self._le_uploadPath.text()
         print(path)
         if len(path) < 1:
@@ -86,6 +86,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print("Cannot Upload")
         self._server_message += self._ftp_client.getServerMessage()
         self.td_server_response.setText(self._server_message)
+
+    # DOWNLOAD BUTTON HANDLER
+    def btn_download_handler(self):
+        path = self.le_download_path.text()
+        if self._loggedIn and self._userAction == 'Download':
+            self._ftp_client.retr(path)
+        else:
+            print('Not logged in')
+        self._server_message += self._ftp_client.getServerMessage()
+
+    def btn_saveto_handler(self):
+        filename, _ = QFileDialog.getOpenFileName(self, 'Save File', '', '', 'All files(*.*)')
 
     # CONNECTION BUTTONS HANDLERS
     def btn_connect_handler(self):
@@ -144,8 +156,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         else:
             print("You are not logged in")
 
-    def btn_moveto_path_handler(self):
-        path = self.le_pathto_moveto.text()
+    # MOVE BUTTON HANDLER
+    def btn_move_handler(self):
+        path = self.le_move.text()
         if path == '':
             self._ftp_client.cdup()
         elif self._loggedIn and self._userAction == 'Move':
@@ -154,6 +167,13 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             print("Did not change directories")
         self._server_message += self._ftp_client.getServerMessage()
         self.td_server_response.setText(self._server_message)
+
+    # PROGRESS BAR UPLOAD
+    def uploadProgress(self):
+        completed = 0
+        while completed < 100:
+            self.completed += 0.0001
+            self.progressBar.setValue(completed)
 
 
 if __name__ == '__main__':
